1bbf74b83087b26f3c8f8b7684260715218abcfd
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-04-30 17:17:20 +0400

    create grid system

--- src/Components/MultiActionAreaCard.js
+++ src/Components/MultiActionAreaCard.js
@@ -4,35 +4,42 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Container, Row, Col } from 'react-grid-system';
 
 export default function MultiActionAreaCard() {
   return (
     <div className='cards'>
-    <Card className='innerCard'>
-    <CardActionArea>
-        <CardMedia
-          component="img"
-          height="110"
-          image="https://goochlandchurches.files.wordpress.com/2019/08/blue.jpg"
-          alt="img"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            <h6>Generate audience</h6>
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Get the customized data about companies, people, and geography you need to get started
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="big" color="primary">
-          Generate
-        </Button>
-      </CardActions>
-      </Card>
+      <Container>
+        <Row>
+          <Col sm={4}>
+           
+            <Card className='innerCard'>
+              <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="110"
+                    image="https://goochlandchurches.files.wordpress.com/2019/08/blue.jpg"
+                    alt="img"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      <h6>Generate audience</h6>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Get the customized data about companies, people, and geography you need to get started
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="big" color="primary">
+                    Generate
+                  </Button>
+                </CardActions>
+            </Card>
+          </Col>
+          <Col sm={4}>
 
-      <Card className='innerCard'> 
+            <Card className='innerCard'> 
     <CardActionArea>
         <CardMedia
           component="img"
@@ -55,30 +62,36 @@ export default function MultiActionAreaCard() {
         </Button>
       </CardActions>
       </Card>
-
-      <Card className='innerCard'> 
-    <CardActionArea>
-        <CardMedia
-          component="img"
-          height="110"
-          image="https://www.myfuturecom.com/wp-content/uploads/2019/01/iStock-952063608-1440x700.jpg"
-          alt="img"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            <h6>Discover predictive features</h6>
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Build and optimize your predictions with impactful external data features, and deploy models or export features
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="big" color="primary">
-          Predict
-        </Button>
-      </CardActions>
-      </Card>
+          </Col>
+          <Col sm={4}>
+            
+            <Card className='innerCard'> 
+          <CardActionArea>
+              <CardMedia
+                component="img"
+                height="110"
+                image="https://www.myfuturecom.com/wp-content/uploads/2019/01/iStock-952063608-1440x700.jpg"
+                alt="img"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  <h6>Discover predictive features</h6>
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                Build and optimize your predictions with impactful external data features, and deploy models or export features
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="big" color="primary">
+                Predict
+              </Button>
+            </CardActions>
+            </Card>
+                </Col>
+              </Row>
+      </Container>
+    
 
 
     </div>
