6436b0b875bba81daf57201bdcf0c6ca82092d4c
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-04-30 18:06:00 +0400

    change images

--- src/Components/MultiActionAreaCard.js
+++ src/Components/MultiActionAreaCard.js
@@ -5,6 +5,9 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import { Container, Row, Col } from 'react-grid-system';
+import audience from '../assets/audience.webp';
+import enrich from '../assets/enrichdata.png';
+import predict from '../assets/predict.jpg';
 
 export default function MultiActionAreaCard() {
   return (
@@ -26,8 +29,8 @@ export default function MultiActionAreaCard() {
               <CardActionArea>
                   <CardMedia
                     component="img"
-                    height="110"
-                    image="https://goochlandchurches.files.wordpress.com/2019/08/blue.jpg"
+                    height="210"
+                    image={audience}
                     alt="img"
                   />
                   <CardContent>
@@ -52,8 +55,8 @@ export default function MultiActionAreaCard() {
     <CardActionArea>
         <CardMedia
           component="img"
-          height="110"
-          image="https://wallpaperboat.com/wp-content/uploads/2020/10/26/58172/lime-green-19.jpg"
+          height="210"
+          image={enrich}
           alt="img"
         />
         <CardContent>
@@ -78,8 +81,8 @@ export default function MultiActionAreaCard() {
           <CardActionArea>
               <CardMedia
                 component="img"
-                height="110"
-                image="https://www.myfuturecom.com/wp-content/uploads/2019/01/iStock-952063608-1440x700.jpg"
+                height="210"
+                image={predict}
                 alt="img"
               />
               <CardContent>
