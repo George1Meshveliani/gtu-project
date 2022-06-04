d2f113f2bde53868e6cd6900dbf596c0f019e280
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-04 22:51:47 +0400

    make corrections

--- src/Pages/EnrichData.js
+++ src/Pages/EnrichData.js
@@ -1,20 +1,20 @@
 import example2 from '../assets/example2.png';
-import scr1 from '../assets/scr1';
-import scr2 from '../assets/scr2';
-import scr3 from '../assets/scr3';
+import scr1 from '../assets/scr1.png';
+import scr2 from '../assets/scr2.png';
+import scr3 from '../assets/scr3.png';
 
 const EnrichData = () => {
     return (
         <div className='main'>
             <h1>Enrich data page</h1>
-            <div className='img'>
-            <img src={example2}/>
-            </div>
-            <h2>Results of our enrichment algorith</h2>
+            <h2>Results of our enrichment algorithm</h2>
             <div className='img'>
             <img src={scr1}/>
             <img src={scr2}/>
             <img src={scr3}/>
+            </div>
+            <div className='img'>
+            <img src={example2}/>
             </div>
         </div>
     )
