cf57366116e0d79ad28b504eeec6f574252b774a
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-04 21:17:22 +0400

    add example page

--- src/Pages/GenerateData.js
+++ src/Pages/GenerateData.js
@@ -1,8 +1,14 @@
+import example from '../assets/example.png'
 
 const GenerateData = () => {
     return (
         <div className='main'>
             <h1>Generation Page</h1>
+            <h2>Example</h2>
+            <div className='img'>
+            <img src={example}/>
+            </div>
+        
         </div>
     )
 } 
