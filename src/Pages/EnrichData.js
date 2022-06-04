393e58ce56f87e1ba8a5a8d323ff8785e343fbdd
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-04 21:23:49 +0400

    add examples

--- src/Pages/EnrichData.js
+++ src/Pages/EnrichData.js
@@ -1,7 +1,13 @@
+import example2 from '../assets/example2.png'
+
+
 const EnrichData = () => {
     return (
         <div className='main'>
             <h1>Enrich data page</h1>
+            <div className='img'>
+            <img src={example2}/>
+            </div>
         </div>
     )
 } 
--- src/Pages/PredictDataPage.js
+++ src/Pages/PredictDataPage.js
@@ -1,7 +1,10 @@
+import example3 from '../assets/example3.png'
+
 const PredictDataPage = () => {
     return (
         <div className='main'>
             <h1>Predict data ...</h1>
+            <img src={example3}/>
         </div>
     )
 } 
